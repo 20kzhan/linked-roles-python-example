8dc988dde56a148595ec99fd4edc8724d612cec4
20kzhan <49377371+20kzhan@users.noreply.github.com>
2023-10-19 22:49:19 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+DISCORD_CLIENT_ID = 12345
+DISCORD_CLIENT_SECRET = "abcdefg"
+DISCORD_TOKEN = "abcdefg"
+DISCORD_REDIRECT_URI = "https://discord.com/discord-oauth-callback"
+COOKIE_SECRET = "a-b-c-e-d"
